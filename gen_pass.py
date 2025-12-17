584354529fec30e4d19f18280569729c20f205d7
sunnatilla <93567759+sunnat19@users.noreply.github.com>
2025-12-18 00:53:08 +0500

    Delete gen_pass.py

--- gen_pass.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-# Настройки как в вашем проекте
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# Генерация хеша
-print(pwd_context.hash("Ваше_пароль"))
-
-# Проверка пароля
