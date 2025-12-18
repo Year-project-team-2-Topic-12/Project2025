0fac59c1074e64e585a2fe6d6502ef72eb8d42e9
sunnatilla <93567759+sunnat19@users.noreply.github.com>
2025-12-18 23:53:27 +0500

    Update main.py

--- Authn/main.py
+++ Authn/main.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
-from routers import history
+from sqlalchemy.orm import Session
+from database import engine
+import crud
+from routers import auth, history
 
 app = FastAPI(
     title="MURA Classifier API",
-    description="API для хранения и просмотра истории запросов",
-    version="1.0.0"
+    description="ML-сервис с авторизацией (JWT) и историей запросов.",
+    version="2.0.0"
 )
 
+# --- ПОДКЛЮЧЕНИЕ РОУТЕРОВ ---
+# 1. Роутер авторизации (/auth/login)
+app.include_router(auth.router)
+
+# 2. Роутер истории (/history)
 app.include_router(history.router)
 
 
-# для тестовой записи логов при запуске
-from database import engine
-from sqlalchemy.orm import Session as OrmSession
-import crud
-
-with OrmSession(engine) as session:
-    crud.add_test_log(session)
+# --- СОБЫТИЯ ЗАПУСКА ---
+@app.on_event("startup")
+def startup_event():
+    with Session(engine) as session:
+        crud.add_test_log(session)
+        print("--- Сервер запущен. Тестовый лог создан. ---")
