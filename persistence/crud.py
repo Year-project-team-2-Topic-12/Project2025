7d9bcb197774eb89bed285d4dd02f6c2720f56d8
paarthurnax42 <dmitry.ivanov42@gmail.com>
2025-12-21 21:07:21 +0300

    Добавлен запрос /stats (#15)

--- persistence/crud.py
+++ persistence/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from models import RequestLog
 from sqlalchemy import select
+from datetime import datetime, timezone
 
 def get_all_logs(session: Session):
     result = session.execute(select(RequestLog))
@@ -19,10 +20,16 @@ def add_log(session: Session, log_data: dict):
 
 def add_test_log(session):
     new_log = RequestLog(
+        timestamp=datetime.now(timezone.utc),
         input_meta="тестовые данные",
+        image_width=2048,
+        image_height=1536,
         duration=0.1,
         result="успех",
         status=200
     )
     session.add(new_log)
     session.commit()
+
+def get_successful_logs(session: Session):
+    return session.query(RequestLog).filter(RequestLog.status == 200).all()
